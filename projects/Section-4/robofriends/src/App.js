fa97b683cc8fe413ce593d56ec8340bd37c632bc
Michael Walter <michael_patrick.walter@outlook.com>
2021-05-26 15:49:01 -0700

    Section-4: robofriends, updated file structure.

--- projects/Section-4/robofriends/src/App.js
+++ projects/Section-4/robofriends/src/App.js
@@ -1,34 +1,13 @@
 import './App.css';
-import axios from 'axios';
-import {useState, useEffect } from 'react';
-import Card from './components/Card';
-
+import CardList from './components/CardList';
 
 // This is test text to update the commit 
 const App = () => {
-  const [robots, setRobots] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get('http://jsonplaceholder.typicode.com/users')
-      .then(res => setRobots(res.data))
-  },[])
 
   return (
     <div className="App">
       <h1 className="f1">AlienFriends</h1>
-      {
-        robots.map((robot,idx) => {
-          return (
-            <Card 
-              name={robot.name} 
-              username={robot.username} 
-              email={robot.email} 
-              id={idx}
-            />
-          )
-        })
-      }
+      <CardList />
     </div>
   );
 }
