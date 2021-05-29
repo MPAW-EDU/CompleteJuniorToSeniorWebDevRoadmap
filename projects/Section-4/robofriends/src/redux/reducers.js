7c4af0294256b244089c9e39ab88c2a483aedd2d
Michael Walter <michael_patrick.walter@outlook.com>
2021-05-29 13:06:07 -0700

    Section-4: robofriends, finished setting up redux and will now start connecting it to the app components.

--- projects/Section-4/robofriends/src/redux/reducers.js
+++ projects/Section-4/robofriends/src/redux/reducers.js
@@ -1,14 +1,42 @@
 
-import CHANGE_SEARCHFIELD from './constants';
+import { 
+    CHANGE_SEARCHFIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+} from './constants';
 
 const initialState = {
     searchField: ''
+}
+
+const initialStateAliens = {
+    isPending: false,
+    aliens: [],
+    error: ''
 }
 
 export const searchAliens = (state=initialState, action={}) => {
     switch(action.type) {
         case CHANGE_SEARCHFIELD:
             return {...state, searchField: action.payload}
+        default:
+            return state;
+    }
+}
+
+
+export const requestAliens = (state=initialStateAliens, action={}) => {
+    switch(action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, { isPending: true})
+
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({}, state, { robots: action.payload, isPending: false })
+
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, { error: action.payload, isPending: false })
+
         default:
             return state;
     }
