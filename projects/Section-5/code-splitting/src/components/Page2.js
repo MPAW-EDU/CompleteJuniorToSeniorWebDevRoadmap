ec7aa69cc2fbe72235238a9bb2a0fa2de44ee742
Michael Walter <michael_patrick.walter@outlook.com>
2021-06-05 22:50:20 -0700

    Section-5: code-splitting, started building out the files and added a state and a switch to handle page routing. Added basic styles. Starting on code splitting for faster load times and easier loading of pages.

--- projects/Section-5/code-splitting/src/components/Page2.js
+++ projects/Section-5/code-splitting/src/components/Page2.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import logo from '../logo.svg'
 
-const Page2 = () => {
+const Page2 = ({onRouteChange}) => {
     return (
-        <div>
-            
+        <div className="APP">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Page2</h1>
+            </header>
+            <button onClick={() => onRouteChange('page1')}>Page1</button>
+            <button className="disabled">Page2</button>
+            <button onClick={() => onRouteChange('page3')}>Page3</button>
         </div>
     )
 }
 
-export default Page2
+export default Page2;
