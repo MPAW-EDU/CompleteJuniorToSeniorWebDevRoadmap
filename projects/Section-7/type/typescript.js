34fdffad96db2bd87a619060136754709afee6b9
Michael Walter <michael_patrick.walter@outlook.com>
2021-06-18 16:43:54 -0700

    Section-7: Worked through additional data types and assertions.

--- projects/Section-7/type/typescript.js
+++ projects/Section-7/type/typescript.js
@@ -20,3 +20,37 @@ var Wizard = {
 // Null and Undefined
 var wah = undefined;
 var wahhahh = null;
+// Tuple 
+var basket;
+basket = ['basketball', 5];
+// Enum 
+var Size;
+(function (Size) {
+    Size[Size["small"] = 1] = "small";
+    Size[Size["medium"] = 2] = "medium";
+    Size[Size["large"] = 3] = "large";
+})(Size || (Size = {}));
+;
+var sizeName = Size[2];
+var SizeName2 = Size.small;
+// Any - !! Take Caution !!
+var whatever = 'Arrrgg!';
+whatever = Size.small;
+// Void
+var sing = function () {
+    console.log('LalaLala!');
+};
+// Never
+var error = function () {
+    throw Error('Ooops!');
+};
+var fightRobotArmy = function (robots) {
+    console.log('FIGHT!');
+};
+// Using a question mark asserts that it may or may not be part of an instantiation of the object.
+var fightAlienArmy = function (alien) {
+    console.log('MMMGRRRGGLLE!!');
+};
+fightAlienArmy({ count: 1, type: 'Hydra' });
+var Alien = {};
+Alien.count;
--- projects/Section-7/type/typescript.ts
+++ projects/Section-7/type/typescript.ts
@@ -28,3 +28,54 @@ let Wizard: object = {
 // Null and Undefined
 let wah: undefined = undefined;
 let wahhahh: null = null;
+
+// Tuple 
+let basket : [string, number];
+basket = ['basketball', 5]
+
+// Enum 
+enum Size { small = 1, medium = 2, large = 3};
+let sizeName: string = Size[2]
+let SizeName2: number = Size.small;
+
+// Any - !! Take Caution !!
+let whatever: any = 'Arrrgg!';
+whatever = Size.small;
+
+// Void
+let sing = (): void => {
+    console.log('LalaLala!');
+}
+
+// Never
+let error = (): never => {
+    throw Error('Ooops!');
+}
+
+// Interface
+interface RobotArmy {
+    count: number,
+    type: string,
+    magic: string
+}
+
+let fightRobotArmy = (robots: RobotArmy) => {
+    console.log('FIGHT!');
+}
+
+// Type Assertions
+interface AlienArmy {
+    count: number,
+    type: string,
+    magic?: string
+}
+// Using a question mark asserts that it may or may not be part of an instantiation of the object.
+let fightAlienArmy = (alien: AlienArmy) => {
+    console.log('MMMGRRRGGLLE!!');
+    
+}
+
+fightAlienArmy({count: 1, type: 'Hydra'})
+
+let Alien = {} as AlienArmy
+Alien.count
