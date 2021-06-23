8cfbde79eb30929f113a9b92d19cbfe1d2572d63
Michael Walter <michael_patrick.walter@outlook.com>
2021-06-23 13:02:46 -0700

    Section-9: Added a note and description detailing how to implement cors to whitelist domains that will access our resources.

--- projects/Section-9/security-files/security-server-exercise/server.js
+++ projects/Section-9/security-files/security-server-exercise/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
-const winston = require('winston');
+const winston = require('winston'); // Recommended for production
 const morgan = require('morgan');
 
 // Read doc for winston and morgan implementations.
@@ -11,8 +11,32 @@ const app = express()
 app.use(cors())
 app.use(helmet())
 app.use(bodyParser.json())
+// app.use(morgan('combined'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+/**
+ * @description Whitelisting domains using CORS options.
+ * 
+ * const whitelist = ['https://examples1.com','https://example2.com']
+ * 
+ * const corsOptions = {
+ *  origin: function ( origin, callback ) {
+ *    if (whitelist.indexOf(origin) !== -1 ) {
+ *      callback( null, true )
+ *    } else {
+ *      callback( new Error('Not allowed by CORS))
+ *    }
+ *  }
+ * } 
+ */
+
+app.get('/', (req, res) => {
+  res.cookie('session', '1', { httpOnly: true })
+  res.cookie('session', '1', { secure: true })
+  res.set({
+    'Content-Security-Policy': "script-src 'self' 'https://apis.google.com'"
+  })
+  res.send('Hello World!')
+})
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
