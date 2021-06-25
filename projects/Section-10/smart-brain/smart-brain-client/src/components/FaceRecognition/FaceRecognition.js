66a1dc49f265a5dad78ffbce189f37e6a31ab5ac
Michael Walter <michael_patrick.walter@outlook.com>
2021-06-25 11:30:35 -0700

    Section-10: face-recognition, fixed key issue with mapping in FaceRecognition component.

--- projects/Section-10/smart-brain/smart-brain-client/src/components/FaceRecognition/FaceRecognition.js
+++ projects/Section-10/smart-brain/smart-brain-client/src/components/FaceRecognition/FaceRecognition.js
@@ -8,9 +8,9 @@ const FaceRecognition = ({ imageUrl, boxes }) => {
       <div className='absolute mt2'>
         <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
         {
-          boxes.map(box => {
+          boxes.map((box, idx) => {
             return (
-              <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+              <div key={idx} className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             )
           })
         }
