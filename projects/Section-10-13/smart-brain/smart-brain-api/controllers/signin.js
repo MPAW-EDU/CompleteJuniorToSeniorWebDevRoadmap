28ea9d08d3e7ebb51845826ba856f3012c72c2d0
Michael Walter <michael_patrick.walter@outlook.com>
2021-07-10 23:27:12 -0700

    Section-10-13: Working to add JWT and sessions into smart-brain-api.

--- projects/Section-10-13/smart-brain/smart-brain-api/controllers/signin.js
+++ projects/Section-10-13/smart-brain/smart-brain-api/controllers/signin.js
@@ -1,26 +1,53 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const jwt = require('jsonwebtoken');
+
+const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject('Incorrect user or password');
   }
-  db.select('email', 'hash').from('login')
+  return db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db.select('*').from('users')
           .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
+          .then(user => user[0])
+          .catch(err => Promise.reject('Unable to get user'))
       } else {
-        res.status(400).json('wrong credentials')
+        Promise.reject('wrong credentials')
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => Promise.reject('wrong credentials'))
+}
+
+const getAuthTokenId = () => {
+  console.log('Authorized');
+}
+
+const signToken = (email) => {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, process.env.JWT_SECRET)
+}
+
+const createSessions = (user) => {
+  // Create JWT token and return user data
+  const { email, id } = user;
+  const token = signToken(email)
+}
+
+const signinAutherntication = ( db, bcrypt ) => ( req,res ) => {
+  const { authorization } = req.headers;
+  return authorization ? 
+        getAuthTokenId() : 
+        handleSignin( db, bcrypt, req, res)
+          .then(data => {
+            data.id && data.email ? createSessions(data) : Promise.reject('Unauthorized User', data)
+          })
+          .catch(err => res.status(400).json(err))
 }
 
 module.exports = {
-  handleSignin: handleSignin
+  handleSignin: handleSignin,
+  signinAutherntication: signinAutherntication
 }
